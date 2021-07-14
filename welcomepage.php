4abe6422009050b3a0a19b5ea012d81d57e6bce2
Anannya786 <77951252+Anannya786@users.noreply.github.com>
2021-07-14 23:48:05 +0600

    Add files via upload

---
+++ welcomepage.php
@@ -0,0 +1,23 @@
+<?php
+   session_start();   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>welcome</title>
+</head>
+<body>
+   <?php echo $_SESSION['username']; ?>
+    <h1>welcome,<?php if(isset($_SESSION['username']) ){
+    echo $_SESSION['username'];
+    
+}
+        else{
+            header("Location: logout.php");
+        } ?></h1>
+    
+    <a href="logout.php">Logout</a>
+</body>
+</html>
+© 2021 GitHub, Inc.
